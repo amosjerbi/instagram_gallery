8166127f166ec839725c6ff8ae08192469af8ac6
Amos Jerbi <amos.jerbi@example.com>
2025-02-16 17:00:27 +0200

    Update config and gallery container class

--- js/config.js
+++ js/config.js
@@ -3,9 +3,7 @@ const config = {
     postsPerPage: 50,
     maxPosts: 270,
     apiFields: 'id,caption,media_type,media_url,permalink,timestamp',
-    allowedMediaTypes: ['IMAGE', 'CAROUSEL_ALBUM', 'VIDEO'],
-    defaultImageSize: 'medium',
-    modalTransitionDuration: 300,
+    allowedMediaTypes: ['IMAGE', 'CAROUSEL_ALBUM']
 };
 
 export default config;
